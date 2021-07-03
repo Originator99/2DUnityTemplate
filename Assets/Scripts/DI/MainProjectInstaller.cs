abae50cde973f7e17a0ac97527b2f0f75cb92930
Originator99 <arsalaan.md0077@gmail.com>
2021-07-04 01:23:13 +0530

    init

---
+++ Assets/Scripts/DI/MainProjectInstaller.cs
@@ -0,0 +1,8 @@
+﻿using Zenject;
+using UnityEngine;
+using System;
+public class MainProjectInstaller : MonoInstaller {
+    public override void InstallBindings() {
+        Debug.Log("Project Installer Initialized");
+    }
+}
